01dd17bbebbca44d6a90b6070c3bef9adb319594
Choi Hyo Seok <tennfin1@gmail.com>
2024-03-02 20:15:54 +0900

    Make annotation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/message') // =@Get('/') //API 이름인 듯 하다 Get이라는 메서드임
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //create application biggest root(necessary) mudule = app module
+
   await app.listen(3000);
+  //start on 3000 port
 }
 bootstrap();
