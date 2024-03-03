00fe98e3916f634f6656b404964d1b25fcf4b4f9
Choi Hyo Seok <tennfin1@gmail.com>
2024-03-04 00:21:00 +0900

    Timeline until 1:44:27

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,24 +1,43 @@
-import { Controller, Get, Post, Body, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Query,
+  Delete,
+  Param,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  @Get('/')
-  getAllBoard(@Query('test') test: string): Board[] {
-    console.log(test);
-    return this.boardsService.getAllBoards();
+  //   @Get('/')
+  //   getAllBoard(@Query('testmessage') testmessage: string): Board[] {
+  //     console.log(testmessage);
+  //     return this.boardsService.getAllBoards();
+  //   }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    // Can replace with - @Param() params: string[]
+    return this.boardsService.getBoardByID(id);
   }
 
   @Post()
   createBoard(
     // Can load all body data with : @Body() body...?
-    @Body('title') title: string,
-    @Body('description') description: string,
+    @Body() createBoardDto: CreateBoardDto,
   ): Board {
-    return this.boardsService.createBoard(title, description);
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Delete('/:id')
+  removeBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
   }
 }
 
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -10,7 +11,8 @@ export class BoardsService {
     return this.boards;
   }
 
-  createBoard(title: string, description: string): Board {
+  createBoard(createBoardDto: CreateBoardDto): Board {
+    const { title, description } = createBoardDto;
     const board: Board = {
       // why we create model to create data on db? we will use prisma...?
       id: uuid(),
@@ -18,8 +20,15 @@ export class BoardsService {
       description,
       status: BoardStatus.PUBLIC,
     };
-
     this.boards.push(board);
     return board; //return what we create
+  }
+
+  getBoardByID(id: string): Board {
+    return this.boards.find((board) => board.id === id); //Callback
+  }
+
+  deleteBoard(id: string): void {
+    this.boards = this.boards.filter((board) => board.id !== id);
   }
 }
