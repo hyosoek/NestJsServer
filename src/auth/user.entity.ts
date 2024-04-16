681dcc4727b2cbda79d4cb531451bec419cc797a
Choi Hyo Seok <tennfin1@gmail.com>
2024-04-16 21:59:42 +0900

    Fix unique key constraint until timeline 4:10:46

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,7 +1,18 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @Entity()
+@Unique(['username'])
 export class Account extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
@@ -12,10 +23,24 @@ export class Account extends BaseEntity {
   @Column()
   password: string;
 
+  // why use static?
+  // beacuse when every connection occur, we don't want to make each connection object.
+  // so we use connection Function with static
+  // and... It is just connect for DB(to transfer command)
   static async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
     const user = this.create({ username, password });
-    await this.save(user);
+    // Create method don't connect to DB. just make code object.
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code == '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 
   static async getAllUser(): Promise<Account[]> {
